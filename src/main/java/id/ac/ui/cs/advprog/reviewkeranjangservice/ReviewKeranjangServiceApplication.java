c3b5d30cba015b44b4836b47756a51007d4a91fb
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2024-04-06 12:10:11 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/ReviewKeranjangServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.reviewkeranjangservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewKeranjangServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewKeranjangServiceApplication.class, args);
+    }
+
+}
