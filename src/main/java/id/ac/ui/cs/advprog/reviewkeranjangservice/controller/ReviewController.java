e66b8334bad19afbe1082b84433e60cf4b78985b
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2024-04-17 16:31:21 +0700

    Added Controller for Keranjang and Review

--- src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/controller/ReviewController.java
+++ src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/controller/ReviewController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/review")
-public class HomeController {
+public class ReviewController {
 
     @GetMapping
-    public String renderHomePage() {
-        return "Hello World";
+    public String renderReviewPage() {
+        return "Hello World Review!!";
     }
 
 }
