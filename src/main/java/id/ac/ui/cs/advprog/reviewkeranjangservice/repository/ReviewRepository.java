3491d30832a8e331b2f3184850e22c3506be11f2
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2024-04-24 02:29:42 +0700

    [RED] Add ReviewRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/repository/ReviewRepository.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.reviewkeranjangservice.repository;
+
+import id.ac.ui.cs.advprog.reviewkeranjangservice.model.Review;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReviewRepository {
+    private List<Review> orderData = new ArrayList<>();
+    public Review save(Review review) {
+        return null;
+    }
+
+    public Review findById(String reviewId) {
+        return null;
+    }
+}
