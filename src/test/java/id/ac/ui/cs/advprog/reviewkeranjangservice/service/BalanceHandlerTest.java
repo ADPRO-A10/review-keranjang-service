e28ed8a911b6923da8019cdb761709bec35f969b
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2024-04-23 12:35:18 +0700

    [RED] Added more test BalanceHandler (CoR Design Pattern)

--- src/test/java/id/ac/ui/cs/advprog/reviewkeranjangservice/service/BalanceHandlerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/reviewkeranjangservice/service/BalanceHandlerTest.java
@@ -10,14 +10,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class BalanceHandlerTest {
 
     BalanceHandler balanceHandler;
+    Handler handler;
 
     @BeforeEach
     void setUp() {
         balanceHandler = new BalanceHandler();
+        handler = mock(Handler.class);
     }
 
     @Test
@@ -37,5 +41,16 @@ public class BalanceHandlerTest {
         person.put("totalBiaya", 30000);
 
         assertThrows(IllegalArgumentException.class, () -> balanceHandler.handleRequest(person));
+    }
+
+    @Test
+    void testNextHandleInvokedWhenPassed() {
+        Map<String, Object> person = new HashMap<>();
+        person.put("saldo", 30000);
+        person.put("totalBiaya", 20000);
+
+        balanceHandler.handleRequest(person);
+        assertEquals("PASSED", balanceHandler.getStatus());
+        verify(handler).handleRequest(person);
     }
 }
