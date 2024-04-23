49e80833089f8bd5306c3e0d8d5926969ade511f
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2024-04-24 02:38:45 +0700

    [GREEN] Implement ReviewRepository class

--- src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/repository/ReviewRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/repository/ReviewRepository.java
@@ -6,12 +6,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReviewRepository {
-    private List<Review> orderData = new ArrayList<>();
-    public Review save(Review review) {
-        return null;
-    }
+    private final List<Review> orderData = new ArrayList<>();
+    public Review save (Review review) {
+        int i = 0;
+        for (Review savedReview : orderData) {
+            if (savedReview.getReviewId().equals(review.getReviewId())) {
+                orderData.remove(i);
+                orderData.add(i, review);
+                return review;
+            }
+            i++;
+        }
 
-    public Review findById(String reviewId) {
+        orderData.add(review);
+        return review;
+    }
+    public Review findById (String id) {
+        for (Review savedReview : orderData) {
+            if (savedReview.getReviewId().equals(id)) {
+                return savedReview;
+            }
+        }
         return null;
     }
 }
