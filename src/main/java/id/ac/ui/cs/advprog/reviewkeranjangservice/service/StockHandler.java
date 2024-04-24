08e4d43de2f4f072798ecddfebba06c0d300529a
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2024-04-24 14:40:11 +0700

    [GREEN] Implement StockHandler (CoR Design Pattern)

--- src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/service/StockHandler.java
+++ src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/service/StockHandler.java
@@ -1,15 +1,43 @@
 package id.ac.ui.cs.advprog.reviewkeranjangservice.service;
 
+import org.springframework.web.client.RestTemplate;
+import org.json.JSONObject;
+
 import java.util.Map;
 
 public class StockHandler extends BaseHandler{
+    private final RestTemplate restTemplate;
+
+    public StockHandler(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @Override
     public void handleRequest(Map<String, Object> request) {
+        for (String key : request.keySet()) {
+            int stockKeranjang = (int) request.get(key);
+            int stockPenjual = getStock(key);
+
+            if (stockKeranjang > stockPenjual) {
+                this.setStatus("FAILED");
+                throw new IllegalArgumentException();
+            }
+        }
+
+        this.setStatus("PASSED");
+        if (this.getNextHandler() != null) {
+            this.getNextHandler().handleRequest(request);
+        }
 
     }
 
     public int getStock(String id) {
-        return -1;
+        String produkApi = "34.143.169.251";
+        String apiUrl = "https://" + produkApi + "/api/products/" + id;
+
+        String jsonResponse = restTemplate.getForObject(apiUrl, String.class);
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+
+        return jsonObject.getInt("stokTersedia");
     }
 }
