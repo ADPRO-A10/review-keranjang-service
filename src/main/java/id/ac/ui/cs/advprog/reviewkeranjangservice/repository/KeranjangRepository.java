7434bd5a3735061c65979ff8ad8ff80f3109e039
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2024-05-02 22:17:41 +0700

    [GREEN] Implement KeranjangRepository

--- src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/repository/KeranjangRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/repository/KeranjangRepository.java
@@ -6,12 +6,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KeranjangRepository {
-    private List<Keranjang> keranjangList = new ArrayList<>();
+    private final List<Keranjang> keranjangList = new ArrayList<>();
     public Keranjang save(Keranjang keranjang) {
-        return new Keranjang();
+        int i = 0;
+        for (Keranjang savedKeranjang : keranjangList) {
+            if (savedKeranjang.getId().equals(keranjang.getId())) {
+                keranjangList.remove(i);
+                keranjangList.add(i, keranjang);
+                return keranjang;
+            }
+            i += 1;
+        }
+
+        keranjangList.add(keranjang);
+        return keranjang;
     }
 
     public Keranjang findById(String id) {
-        return new Keranjang();
+        for (Keranjang savedKeranjang : keranjangList) {
+            if (savedKeranjang.getId().equals(id)) {
+                return savedKeranjang;
+            }
+        }
+        return null;
     }
 }
