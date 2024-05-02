0ef6156d34cedb7b52dd080df98e75b3a2431c82
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2024-05-02 14:31:17 +0700

    [RED] Add Keranjang Skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/reviewkeranjangservice/model/Keranjang.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.advprog.reviewkeranjangservice.model;
+
+import java.util.List;
+import java.util.Map;
+
+import java.util.UUID;
+
+
+public class Keranjang {
+    private String id;
+    private List<Map<String, Object>> listProduk;
+
+    public String getId() {
+        return "";
+    }
+
+    public void setId(String id) {
+
+    }
+    public List<Map<String, Object>> getListProduk() {
+        return null;
+    }
+
+    public void setListProduk(List<Map<String, Object>> listProduk) {
+    }
+
+}
